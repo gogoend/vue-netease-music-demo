133d5371c28ce6ca01ee1829723069b1ff54c958
gogoend <gogoend@qq.com>
2020-11-06 00:12:28 +0800

    enhance: 样式微调；ts函数标注报错解决；登录后标题栏理应展示用户头像、昵称

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <template>
   <div id="app" class="app-wrap">
     <header>
-      <h1>……</h1>
+      <h1>Application</h1>
+      <section class="user-profile" v-if="accountInfo">
+        <img :src="accountInfo.profile.avatarUrl" />
+        <span>{{accountInfo.profile.nickname}}</span>
+      </section>
       <section class="search-wrap">
-        <el-input clearable v-model="searchKeyword" /><el-button type="primary">搜索</el-button>
+        <el-input clearable v-model="searchKeyword" /><el-button type="primary"
+          >搜索</el-button
+        >
       </section>
     </header>
     <section class="main-wrap">
@@ -17,24 +23,28 @@
         <router-view class="main-view" />
       </main>
     </section>
-    <footer>
-    </footer>
+    <footer></footer>
   </div>
 </template>
 
 <script lang="ts">
-import { Vue, Component } from 'vue-property-decorator'
+import { Vue, Component } from "vue-property-decorator";
 
-@Component 
-class App extends Vue{
-  private searchKeyword = ''
+import { namespace } from "vuex-class";
+
+const userModule = namespace("user");
+
+@Component
+class App extends Vue {
+  @userModule.Getter("accountInfo") private accountInfo: unknown;
+
+  private searchKeyword = "";
 }
 
-export default App
+export default App;
 </script>
 
 <style lang="less" scoped>
-
 @header-height: 64px;
 @footer-height: 100px;
 
@@ -54,28 +64,29 @@ export default App
     align-items: center;
     flex: 0 0 @header-height;
     background-color: #07f9b1;
-    .search-wrap{
+    .search-wrap {
       display: flex;
     }
   }
-  section.main-wrap{
+  section.main-wrap {
     display: flex;
     flex: 1;
-    max-height: calc( 100% - 164px );
-    nav{
+    max-height: calc(100% - 164px);
+    nav {
       flex: 0 0 200px;
       overflow-y: auto;
+      border-right: 1px solid #828282;
     }
-    main{
+    main {
       flex: 1;
       overflow-y: auto;
-      .main-view{
+      .main-view {
         height: 100%;
       }
     }
   }
-  footer{
-    flex: 0 0 @footer-height
+  footer {
+    flex: 0 0 @footer-height;
   }
 }
 </style>
