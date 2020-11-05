aec821280162efab14820b627716eaee2df5c756
gogoend <gogoend@qq.com>
2020-11-06 00:26:43 +0800

    enhance: 持久化存储vuex

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Vuex, { Commit, Dispatch } from 'vuex'
+import createPersistedState from 'vuex-persistedstate'
 
 export interface ActionContextBasic {
     commit: Commit;
@@ -19,5 +20,10 @@ export default new Vuex.Store({
   },
   modules: {
     user
-  }
+  },
+  plugins: [
+    createPersistedState({
+      storage: window.sessionStorage, overwrite: true
+    })
+  ]
 })
