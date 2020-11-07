1115af6a24cb7bae6e860411d4711deddbe97af1
gogoend <gogoend@qq.com>
2020-11-07 14:45:22 +0800

    enhance: 修复样式

---
+++ src/components/ContentWrap/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div
+    class="content-wrap"
+    :style="{
+      width
+    }"
+  >
+    <slot />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class ContentWrap extends Vue {
+  @Prop({
+    type: [String, Number],
+    default: "80%",
+  })
+  private width!: string | number;
+}
+</script>
+
+<style scoped lang="less">
+.content-wrap {
+  margin: auto;
+  max-width: 720px;
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
---
+++ src/components/MainWrap/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="main-wrap">
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped lang="less">
+.main-wrap {
+  padding: 20px;
+  height: 100%;
+  box-sizing: border-box;
+}
+</style>
